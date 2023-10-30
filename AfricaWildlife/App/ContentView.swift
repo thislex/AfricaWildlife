938c3432f38325bb5344e8fb22c00416d54ef869
Lexter Tapawan <lexter.tapawan@gmail.com>
2023-10-30 21:49:20 +0000

    Implemented new detail and list views, aling with scrolling functions.

--- AfricaWildlife/App/ContentView.swift
+++ AfricaWildlife/App/ContentView.swift
@@ -11,12 +11,20 @@ struct ContentView: View {
     var body: some View {
         // MARK: - PROPERTIES
         
+        let animals: [Animal] = Bundle.main.decode("animals.json")
+        
         // MARK: - BODY
         NavigationView {
             List {
                 CoverImageView()
                     .frame(height: 300)
                     .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                
+                ForEach(animals) { animal in
+                    NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                        AnimalListItemView(animal: animal)
+                    } //: LINK
+                } //: LOOP
             } //: LIST
             .navigationTitle("Africa")
             .navigationBarTitleDisplayMode(.large)
---
+++ AfricaWildlife/View/AnimalDetailView.swift
@@ -0,0 +1,76 @@
+//
+//  AnimalDetailView.swift
+//  AfricaWildlife
+//
+//  Created by Lexter Tapawan on 30/10/2023.
+//
+
+import SwiftUI
+
+struct AnimalDetailView: View {
+    // MARK: - PROPERTIES
+    
+    let animal: Animal
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 20) {
+                // HERO IMAGE
+                Image(animal.image)
+                    .resizable()
+                    .scaledToFit()
+                
+                // TITLE
+                Text(animal.name.uppercased())
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .multilineTextAlignment(.center)
+                    .padding(.vertical, 8)
+                    .foregroundStyle(.primary)
+                    .background(
+                        Color.accentColor
+                            .frame(height: 6)
+                            .offset(y: 24)
+                    )
+                
+                // HEADLINE
+                Text(animal.headline)
+                    .font(.headline)
+                    .multilineTextAlignment(.leading)
+                    .foregroundStyle(.accent)
+                    .padding(.horizontal)
+                
+                // GALLERY
+                Group {
+                    HeadingView(headingImage: "photo.on.rectangle.angled", headingText: "Wilderness in Pictures")
+                    
+                    InsetGalleryView(animal: animal)
+                }
+                .padding(.horizontal)
+                
+                // FACTS
+                
+                // DESCRIPTION
+                
+                // MAP
+                
+                //LINK
+                
+            } //: VSTACK
+            .navigationTitle("Learn about \(animal.name)")
+            .navigationBarTitleDisplayMode(.inline)
+        } //: SCROLL
+    }
+}
+
+// MARK: - PREVIEW
+
+#Preview {
+@State var animals: [Animal] = Bundle.main.decode("animals.json")
+
+return AnimalDetailView(animal: animals[0])
+
+
+}
