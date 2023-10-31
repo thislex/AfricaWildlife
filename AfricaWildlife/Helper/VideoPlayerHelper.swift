1ceb06a6db9b96aec56d5191725d8ef485d1c58f
Lexter Tapawan <lexter.tapawan@gmail.com>
2023-10-31 11:08:20 +0000

    Video player implemented.

---
+++ AfricaWildlife/Helper/VideoPlayerHelper.swift
@@ -0,0 +1,19 @@
+//
+//  VideoPlayerHelper.swift
+//  AfricaWildlife
+//
+//  Created by Lexter Tapawan on 31/10/2023.
+//
+
+import Foundation
+import AVKit
+
+var videoPlayer: AVPlayer?
+
+func playVideo(fileName: String, fileFormat: String) -> AVPlayer {
+    if Bundle.main.url(forResource: fileName, withExtension: fileFormat) != nil {
+        videoPlayer = AVPlayer(url: Bundle.main.url(forResource: fileName, withExtension: fileFormat)!)
+        videoPlayer?.play()
+    }
+    return videoPlayer!
+}
